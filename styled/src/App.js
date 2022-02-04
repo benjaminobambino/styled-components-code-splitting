3589deb9dfa32e83071616b00ecff0dfb0deee32
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-04 15:13:25 -0800

    learned and used styled components

--- styled/src/App.js
+++ styled/src/App.js
@@ -1,10 +1,47 @@
+import styled, { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './themes';
+import { GlobalStyles } from './GlobalStyles';
+import { useState } from 'react';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  )
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <div>
+        <h1>Hello World</h1>
+        <Button onClick={() => themeToggler()}>Change Theme</Button>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+const Wrapper = styled.div`
+  font-family: Helvetica;
+`;
+
+const Button = styled.button`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  border: 2px solid ${(props) => props.theme.fontColor};
+  cursor: pointer;
+  transition: 0.2s;
+
+  color: ${(props) => props.theme.fontColor};
+  background-color: ${(props) => props.theme.body};
+
+  &:hover {
+    background-color: ${(props) => props.theme.fontColor};
+    color: ${(props) => props.theme.body};
+  }
+`;
+
+export default App;
